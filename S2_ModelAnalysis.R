e8ec33592a500390e57d92360e784d3723ddc4ce
Sam Perrin <sam.perrin@ntnu.no>
2020-10-27 13:54:08 +0100

    Added initial scripts

---
+++ S2_ModelAnalysis.R
@@ -0,0 +1,320 @@
+#----------------------#
+### 1. MODEL IMPORT ####
+#----------------------#
+
+# Set parameters
+reduced_species <- TRUE                # All species in model or only those in >10% of lakes?
+spatial_component <- FALSE             # Introduce spatial component?
+association <- TRUE                    # Estimate associations over temperature?
+
+# Load libraries
+library(greta)
+library(tensorflow)
+library(dplyr)
+library(corrplot)
+library(ggplot2)
+library(magrittr)
+library(gridExtra)
+
+
+# Import model and scripts
+NOFA_JSAM_results <- readRDS("ModelFile.RDS")
+source("JSAM_Functions.R")
+
+# Define data and parameters
+NOFA_Data <- NOFA_JSAM_results$data
+
+model <- NOFA_JSAM_results$model
+parameters <- NOFA_JSAM_results$parameters
+
+Y <- NOFA_Data$Y[,parameters$species_names]
+
+# Define model components
+draws <- model$draws
+R_lower <- model$R_lower
+R <- model$R_ident
+beta <- model$beta
+alpha <- model$alpha
+p <- model$p
+lambda_int <- model$lambda_int
+R_pred_ident <- model$R_pred_ident
+valley_offset <- model$valley_offset
+  
+  
+n_species <- parameters$n_species
+valid_species <- parameters$valid_species
+
+temp_sd <- 10/sd(NOFA_Data$full_data$eurolst_bio10)
+X_assoc_pred <- parameters$X_assoc_pred
+X_assoc_pred_nonSD <- round(mean(NOFA_Data$full_data$eurolst_bio10) + 
+                              X_assoc_pred * sd(NOFA_Data$full_data$eurolst_bio10),1)/10
+#X_assoc_pred_test <- seq(-3.5,2.5,length.out=11)
+
+#---------------------#
+### 2. MODEL CHECK ####
+#---------------------#
+
+# Check convergence
+
+calc_R <- calculate(R_lower,values=draws)
+gelman_sims_R <- coda::gelman.diag(calc_R,multivariate = FALSE)
+nrow(gelman_sims_R$psrf[gelman_sims_R$psrf[,1] < 1.1,])/nrow(gelman_sims_R$psrf)
+
+calc_beta <- calculate(beta,values=draws)
+gelman_sims_beta <- coda::gelman.diag(calc_beta,multivariate = FALSE)
+nrow(gelman_sims_beta$psrf[gelman_sims_beta$psrf[,1] < 1.1,])/nrow(gelman_sims_beta$psrf)
+
+if (spatial_component == TRUE) {
+  calc_catchment <- calculate(valley,values=draws)
+  gelman_sims_catchment <- coda::gelman.diag(calc_catchment,multivariate = FALSE)
+  nrow(gelman_sims_catchment$psrf[gelman_sims_catchment$psrf[,1] < 1.1,])/nrow(gelman_sims_catchment$psrf)
+}
+
+
+#----------------------------#
+### 3. Parameter Analysis ####
+#----------------------------#
+
+nofa_beta_list <- get_beta_list(draws,beta_shared = beta,species_names=parameters$species_names,
+                                env_names=parameters$env_names)
+
+nofa_beta_list$significance <- as.factor(ifelse(nofa_beta_list$lower > 0 | nofa_beta_list$upper < 0, 0,1))
+nofa_beta_sig <- filter(nofa_beta_list,
+                        significance == 0)
+filter(nofa_beta_sig,variable=="temperature")
+
+
+ggplot(nofa_beta_list, aes(x=variable, y=mean,col=variable, linetype = significance)) + 
+  ylim(-4,4) +
+  geom_linerange(aes(ymin=lower, ymax=upper)) +
+  facet_wrap(~ species) +
+  theme(axis.title.x=element_blank(),
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank()) + 
+  geom_hline(yintercept=0)
+
+ggplot(nofa_beta_list, aes(x=species, y=mean,col=species, linetype = significance)) + 
+  ylim(-2,2) +
+  geom_linerange(aes(ymin=lower, ymax=upper)) +
+  facet_wrap(~ variable) +
+  theme(axis.title.x=element_blank(),
+        axis.text.x=element_blank(), 
+        axis.ticks.x=element_blank()) + 
+  geom_hline(yintercept=0)
+
+# # Check if and valley values are significant
+# valley_mean <- matrix(apply(as.matrix(calc_catchment),2, mean), 
+#                       nrow = nrow(valley), ncol = ncol(valley))
+# valley_lower <- matrix(apply(as.matrix(calc_catchment),2, quantile, probs = 0.025), 
+#                        nrow = nrow(valley), ncol = ncol(valley))
+# valley_upper <- matrix(apply(as.matrix(calc_catchment),2, quantile, probs = 0.975), 
+#                        nrow = nrow(valley), ncol = ncol(valley))
+# valley_values <- valley_mean
+# 
+# valley_values[valley_lower < 0 & valley_upper > 0] <- 0
+
+#------------------------------#
+### C. Corellation Analysis ####
+#-------------------..---------#
+
+# Get correlaton matrix at mean temperature first
+nofa_cooccurrence_R <- get_cooc_ints(draws,parameters$species_names, R=R)
+diag(nofa_cooccurrence_R) <- 0
+nofa_cooccurrence_R_lower <- get_cooc_ints(draws,parameters$species_names,
+                                           R, quantile=0.025)
+nofa_cooccurrence_R_upper <- get_cooc_ints(draws,parameters$species_names,
+                                           R, quantile=0.975)
+# Stipulate species order early on
+species_order <- row.names(nofa_cooccurrence_R)
+common_names <- c("Burbot", "Roach", "Zander", "Bleak", "Crucian carp", "Perch","Vendace", "Bream",
+                  "Pike", "Whitefish", "Rudd", "Tench", "Ruffe", "Brown trout", "Artic charr")
+
+# Now let's show species with significant associations
+nofa_cooccurrence_sig <- nofa_cooccurrence_R
+nofa_cooccurrence_sig[nofa_cooccurrence_R_lower < 0 & nofa_cooccurrence_R_upper > 0] <- 0
+corrplot(nofa_cooccurrence_R, method = "color",type="lower",order="original", diag=FALSE,
+         cl.lim=c(-1,1))
+corrplot(nofa_cooccurrence_sig, method = "color",type="lower", order="original",diag=FALSE,
+         cl.lim=c(-1,1))
+
+
+
+# Now map correlations in species over the temperature gradients
+nofa_correlation_temp <- pred_correlation(draws,X_assoc_pred,parameters$species_names,R_pred_ident)
+nofa_correlation_temp_lower <- pred_correlation(draws,X_assoc_pred,
+                                                parameters$species_names,R_pred_ident,
+                                                quantile_n=0.025)
+nofa_correlation_temp_upper <- pred_correlation(draws,X_assoc_pred,
+                                                parameters$species_names,R_pred_ident,
+                                                quantile_n=0.975)
+
+# Save the above 3 lists to speed up future analysis
+correlation_CIs <- list(lower = nofa_correlation_temp_lower, mean = nofa_correlation_temp,
+                        upper = nofa_correlation_temp_upper)
+
+# Isolate correlation matrices for associations at specific temperatures
+# for easier visualisation
+NCT_mean <- nofa_correlation_temp[[7]][species_order,species_order] %>%
+  set_colnames(common_names) %>%
+  set_rownames(common_names)
+NCT_low <- nofa_correlation_temp[[1]][species_order,species_order] %>%
+  set_colnames(common_names) %>%
+  set_rownames(common_names)
+NCT_high <- nofa_correlation_temp[[11]][species_order,species_order] %>%
+  set_colnames(common_names) %>%
+  set_rownames(common_names)
+
+# Plto associations at low, high and mean values
+corrplot(NCT_mean, method = "color",type="upper",order="original",diag=FALSE, tl.col="black",
+         cl.lim=c(-1,1),is.corr=FALSE,mar=c(0,0,1,1))
+corrplot(NCT_low, method = "color",type="upper",order="original",diag=FALSE, tl.col="black",
+         cl.lim=c(-1,1),is.corr=FALSE,mar=c(0,0,1,1))
+corrplot(NCT_high, method = "color",type="upper",order="original",diag=FALSE, tl.col="black",
+         cl.lim=c(-1,1),is.corr=FALSE,mar=c(0,0,1,1))
+
+
+
+# The following function shows associations between 2 species
+view_interaction <- function(interacting_species, correlation_CIs, X_assoc_pred, ylim_p = c(-1,1), title.int=NA,
+                             xlab.int = NA, ylab.int=NA) {
+  
+  all_assocs <- create_all_association_gradient(correlation_CIs$mean)
+  all_assocs_lower <- create_all_association_gradient(correlation_CIs$lower)
+  all_assocs_higher <- create_all_association_gradient(correlation_CIs$upper)
+  
+  interacting_species_frame <- as.data.frame(matrix(NA, nrow = length(X_assoc_pred), ncol = 4))
+  interacting_species_frame[,1] <- X_assoc_pred_nonSD
+  interacting_species_frame[,2] <- all_assocs_lower %>%
+    filter(species1 == interacting_species[1] & species2 == interacting_species[2]) %>%
+    select(1:length(X_assoc_pred)) %>% t()
+  interacting_species_frame[,3] <- all_assocs %>%
+    filter(species1 == interacting_species[1] & species2 == interacting_species[2]) %>%
+    select(1:length(X_assoc_pred)) %>% t()
+  interacting_species_frame[,4] <- all_assocs_higher %>%
+    filter(species1 == interacting_species[1] & species2 == interacting_species[2]) %>%
+    select(1:length(X_assoc_pred)) %>% t()
+  colnames(interacting_species_frame) <- c("temp","low","mean","high")
+  
+  D0 <- ggplot(interacting_species_frame, aes(temp, mean)) +
+    geom_line(aes(colour = "red"), # Line type depends on cond
+              size = 1.5) +
+    ylim(ylim_p) +
+    geom_ribbon(aes(ymin = low, ymax = high, fill = "red"), alpha = .35) +
+    theme_bw() +
+    theme(legend.position = "none") + 
+    scale_x_continuous(breaks=seq(7.5,16.5,3))
+  if (is.na(title.int)) {D1 <- D0 + 
+    labs(title = paste0("Relationship between ",interacting_species[1],
+                        " and ",interacting_species[2]))} else {
+                          D1 <- D0 + labs(title = title.int)
+                        }
+  
+  if (is.na(xlab.int)) {
+    D2 <- D1 +
+      xlab("Standardised temperature")
+  } else {
+    D2 <- D1 +
+      xlab(xlab.int) 
+  }
+  
+  if (is.na(ylab.int)) {
+    D3 <- D2 +
+      ylab("Association")
+  } else {
+    D3 <- D2 + ylab(ylab.int)
+  }
+  D3
+}
+
+# Example
+view_interaction(c("Rutilus_rutilus","Lota_lota"), correlation_CIs, X_assoc_pred, title = "")
+
+
+
+# Calculate correlation in environmental responses
+beta_mat <- as.matrix(calc_beta)
+all_enviro_cor_mat <- array(0, dim = c(nrow(beta_mat), n_species, n_species))
+
+env_matrix <- as.matrix(cbind(NOFA_Data$X[,-1], NOFA_Data$X$temperature^2)) # all env
+
+# This uses the same method of calculation as Francis Hui's boral package.
+for (i in 1:nrow(draws$`11`)) {
+  cw_X_coefs <- matrix(beta_mat[i,],nrow=n_species)
+  enviro.linpreds <- tcrossprod(env_matrix, as.matrix(cw_X_coefs[,c(1:7,8)]))
+  #  enviro.linpreds <- tcrossprod(env_matrix, as.matrix(cw_X_coefs[,c(2,9)]))
+  all_enviro_cor_mat[i,,] <- cor(enviro.linpreds)
+}
+
+enviro_cor_mat <- matrix(0, n_species, n_species)
+
+for (j in 1:n_species) {
+  for (j2 in 1:n_species) {
+    enviro_cor_mat[j, j2] <- mean(all_enviro_cor_mat[, j, j2])
+  }
+}
+colnames(enviro_cor_mat) <- colnames(Y)
+rownames(enviro_cor_mat) <- colnames(Y)
+
+enviro_cor_mat_ordered <- enviro_cor_mat[species_order, species_order] %>%
+  set_colnames(common_names) %>%
+  set_rownames(common_names)
+
+enviro_cor_plot <- corrplot(enviro_cor_mat_ordered, method = "color",type="upper",order="original",diag=FALSE,
+                            tl.col='black',mar=c(0,0,1,1))
+
+
+#-------------------#
+### D. Model fit ####
+#-------------------#
+
+
+
+### Calculating deviance
+
+# Now we look at deviance
+Y_dev <- NOFA_Data$full_data[,parameters$species_names]
+
+# intercept-only NULL deviance
+p_null <- colMeans(Y_dev)
+p_null <- kronecker(rep(1, parameters$n_sites), t(p_null))
+deviance_yp(as.matrix(Y_dev),p_null)
+
+# environment-only deviance
+dev_env <- 0
+for (i in 1:n_species) {
+  dat <- data.frame(y = Y_dev[, i],
+                    NOFA_Data$full_data[,parameters$env_names])
+  m <- glm(y ~ ., data = dat,
+           family = stats::binomial("probit"))
+  pred <- predict(m, type = "response")
+  dev_env <- dev_env + deviance_yp(dat$y, pred)
+}
+dev_env
+
+
+
+# Model deviance
+# Note: THis is a fairly time-consuming process, and it takes a lot of memory.
+# I recommend deleting any large files you no longer need from your R workspace before
+# starting it.
+p_mat <- matrix(ncol=ncol(p),nrow=nrow(p))
+
+time1 <- Sys.time()
+for (i in 1:nrow(p_mat)) {
+  p_temp <- calculate(p[,i], values = draws)
+  p_out <- as.matrix(p_temp)
+  rm(p_temp)
+  p_out_medians <- apply(p_out,2,median)
+  rm(p_out)
+  p_mat[,i] <-p_out_medians
+  
+  # Quick function to let us know how the loop is progressing
+  time2 <- Sys.time()
+  difftime_1 <- round(as.numeric(difftime(time2, time1,
+                                          units = "mins")),4)
+  if (i %% 3 == 0) {print(paste0("Run ", i, " of ",  nrow(p_reduced), " finished in ",difftime_1, " minutes. Estimated ", round(difftime_1*nrow(p_reduced)/i-difftime_1,3), " minutes left.") )}
+}
+
+deviance_yp(as.matrix(Y_dev),p_mat)
+
+saveRDS(p_mat, file = "./CodesAndData/reducedmodel_devianceValues.RDS")
---
+++ S2_ModelConstruct.R
@@ -0,0 +1,194 @@
+#-------------------------#
+### 1. MODEL CONSTRUCT ####
+#-------------------------#
+
+# Set parameters
+reduced_species <- TRUE                # All species in model or only those in >10% of lakes?
+spatial_component <- FALSE             # Introduce spatial component?
+association <- TRUE                    # Estimate associations over temperature (FALSE implies 
+                                       # just measured at mean temperature)?
+
+#---------------------------#
+### A. Data Construction ####
+#---------------------------#
+
+# Data import
+library(greta)
+library(dplyr)
+load("./CodesAndData/NOFA_Data_WC.rda")
+source("JSAM_Functions.R")
+
+# Colinnearity check
+source("http://www.sthda.com/upload/rquery_cormat.r")
+colin <- rquery.cormat(NOFA_Data$X, type = "flatten", graph = FALSE)
+colin$r %>% filter(abs(cor) > 0.4)
+
+# Check to see data distribution of anything suspicious on collinearity
+with(NOFA_Data$X,plot(HFP_sc,temperature))
+
+# Remove elevation data
+NOFA_Data$X <- NOFA_Data$X[,-1]
+
+# The first task here is to create a matrix which shows numbers of cooccurrences between different species.
+# The idea being that if they only cooccur below a certain limit, there is insufficient data to
+# gauge associations (Tikhonov et al)
+n_species <- ncol(NOFA_Data$Y)
+
+# Isolate environmental data, get rid of elevation (unnecessary)
+X_base <- NOFA_Data$X %>%
+  mutate(temp_sq = temperature^2)
+
+# Take species data
+if (reduced_species == TRUE) {
+  valid_species <- which(apply(NOFA_Data$Y, 2, mean) > 0.1)
+  Y_base <- NOFA_Data$Y[,valid_species]
+  n_species <- length(valid_species)
+}
+
+# Load spatial component if necessary  
+if (spatial_component == TRUE) {
+  load("./CodesAndData/lake_geom_sf.RDA")
+  sites <- merge(NOFA_Data$full_data["waterBodyID"], dplyr::select(lake_geom_sf,-geometry),
+                 all.x=TRUE, by = "waterBodyID")
+  sites <- sites[,c("waterBodyID","inout")]
+  site_no <- as_data(sites["inout"])
+  n_valleys <- length(unique(site_no))
+}
+
+# Convert main tables to greta
+X <- as_data(as.matrix(X_base))
+Y <- as_data(as.matrix(Y_base))
+
+# Set some parameters
+nofa_env_names <- colnames(X_base)
+nofa_species_names <- colnames(Y_base)
+
+# Set species association vector up
+X_assoc <- as_data(X_base$temperature)
+
+if (association == TRUE) {
+  minXassoc <- floor(min(X_base$temperature)*10)/10
+  maxXassoc <- ceiling(max(X_base$temperature)*10)/10
+  X_assoc_pred <- seq(minXassoc,maxXassoc,length.out=11)
+}
+
+# Define dimensions
+n_sites<- nrow(X)
+n_env <- ncol(X)
+n_latent <- 3
+n_species <- ncol(Y)
+
+# Finally, group parameters for later
+parameters <- list(n_sites = n_sites, n_env = n_env, n_latent=n_latent, n_species=n_species, 
+                   species_names = nofa_species_names, env_names = nofa_env_names,
+                   valid_species = valid_species)
+if (association == TRUE) {parameters[["X_assoc_pred"]] <-  X_assoc_pred}
+if (spatial_component == TRUE) {parameters[["site_no"]] <- site_no}
+
+#-----------------------#
+### B. Model fitting ####
+#-----------------------#
+
+alpha <- normal(-2, 1, dim = n_species)
+beta <- normal(0, 10, dim = c(n_env, n_species))
+
+z <- normal(0, 1, dim = c(n_latent, n_sites))
+  
+# Define our priors for lambda. Lambda_int will create the correlation matrix later on.
+lambda_int <- create_lambda_wide(n_species,n_latent, 10)
+lambda_coef <- create_lambda_wide(n_species,n_latent, 10)
+
+# Hierarchical component
+if (spatial_component == TRUE) {
+  valley_sd <- normal(0, 10, truncation = c(0,Inf))
+  valley_offset_raw <- normal(0, 1, dim = c(n_valleys,n_species))
+  valley_offset <- valley_offset_raw * valley_sd
+  valley_effect <- valley_offset[as.factor(site_no),]
+}
+
+# Now produce our prediction function for our later correlation matrix
+if (association == TRUE) {
+  R_pred_ident <- pred_R_ident(X_assoc_pred,lambda_int=lambda_int, lambda_coef = lambda_coef)
+}
+
+# Now just put the model together. THis can get tricky.
+if (association == TRUE & spatial_component == TRUE) {
+  eta <- pred_eta(X_newdata = X, 
+                  X_assoc_newdata = X_assoc,alpha = alpha,beta=beta,
+                  lambda_int = lambda_int, lambda_coef = lambda_coef,z=z,
+                  catchment_effect = valley_effect, site_id_newdata = site_no)
+} else if (association == TRUE & spatial_component == FALSE) {
+  eta <- pred_eta(X_newdata = X, 
+                  X_assoc_newdata = X_assoc,alpha = alpha,beta=beta,
+                  lambda_int = lambda_int, lambda_coef = lambda_coef,z=z)
+} else if (association == FALSE & spatial_component == TRUE) {
+  eta <- pred_eta_null(X_newdata = X,
+                       X_assoc_newdata = X_assoc, alpha = alpha,beta=beta,
+                       lambda_int = lambda_int,z=z,
+                       catchment_effect = valley_effect, site_id_newdata = site_no)
+} else {
+  eta <- pred_eta_null(X_newdata = X,
+                       X_assoc_newdata = X_assoc, alpha = alpha,beta=beta,
+                       lambda_int = lambda_int,z=z)
+}
+
+
+# Define the distribution
+p <- iprobit(eta)
+distribution(Y) <- bernoulli(p)
+
+# Define our correlation matrix
+R_ident <- greta::cov2cor(lambda_int %*% t(lambda_int) + diag(n_species))
+lower_idx_R <- lower.tri(R_ident)
+R_lower <- R_ident[lower_idx_R]
+
+# Define and plot  model
+nofa_gmodel <- model(beta)
+plot(nofa_gmodel)
+
+# Aaaaand perform MCMC
+Lmin <- 40
+Lmax <- round(Lmin*1.5)
+nofa_Gdraws <- mcmc(nofa_gmodel,chains = 1, n_samples = 3000, warmup = 2000 
+                    ,sampler = hmc(Lmin = Lmin, Lmax = Lmax))
+
+#---------------------------------#
+### C. Model convergence check ####
+#---------------------------------#
+
+calc_R_lower <- calculate(R_lower,values=nofa_Gdraws)
+gelman_sims_R_lower <- coda::gelman.diag(calc_R_lower,multivariate = FALSE)
+nrow(gelman_sims_R_lower$psrf[gelman_sims_R_lower$psrf[,1] < 1.1,])/nrow(gelman_sims_R_lower$psrf)
+
+calc_beta <- calculate(beta,values=nofa_Gdraws)
+gelman_sims_beta <- coda::gelman.diag(calc_beta,multivariate = FALSE)
+nrow(gelman_sims_beta$psrf[gelman_sims_beta$psrf[,1] < 1.1,])/nrow(gelman_sims_beta$psrf)
+
+calc_lambda <- calculate(lambda_coef,values=nofa_Gdraws)
+gelman_sims_lambda <- coda::gelman.diag(calc_lambda,multivariate = FALSE)
+nrow(gelman_sims_lambda$psrf[gelman_sims_lambda$psrf[,1] < 1.1,])/nrow(gelman_sims_lambda$psrf)
+
+
+
+#------------------------------#
+### D. Consolidate and save ####
+#------------------------------#
+
+
+NOFA_JSAM_model_subset <- list(draws = nofa_Gdraws, beta = beta, alpha = alpha, 
+                               R_ident = R_ident, R_lower = R_lower, p = p, 
+                               eta = eta, z = z, lambda_int = lambda_int
+                               )
+if (association == TRUE) {
+  NOFA_JSAM_model_subset[["R_pred_ident"]] <- R_pred_ident
+  NOFA_JSAM_model_subset[["lambda_coef"]] <- lambda_coef
+}
+
+if (spatial_component == TRUE) {
+  NOFA_JSAM_model_subset[["valley_offset"]] <- valley_offset
+}
+
+NOFA_JSAM_subset <- list(model=NOFA_JSAM_model_subset, data=NOFA_Data, parameters=parameters)
+
+file_name <- getPass("Name the file")
+saveRDS(NOFA_JSAM_subset,paste0("./SelectYourRepository/",file_name,".RDS"))
